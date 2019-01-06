f8d2d47a5e3d4cf9bad840b53e714e2954525e35
haesleinhuepf <rhaase@mpi-cbg.de>
2019-01-06 17:18:39 +0100

    cleaned up code; prevented Java Heap explosion

--- src/test/java/net/haesleinhuepf/clij/histogramplugin/HistogramBenchmarking.java
+++ src/test/java/net/haesleinhuepf/clij/histogramplugin/HistogramBenchmarking.java
@@ -25,51 +25,56 @@ public class HistogramBenchmarking {
 
     @Test
     public void testPerformanceOfGPUBasesHistogramGeneration() {
+        int imageWidth = 1024;
+        int imageHeight = 1024;
+        int imageDepth = 50;
+
+        long[] referenceHistogram = new long[256];
+        referenceHistogram[2] = 34;
+        referenceHistogram[100] = 5;
+        referenceHistogram[145] = 22;
+        referenceHistogram[0] = imageWidth * imageHeight * imageDepth - sumArray(referenceHistogram);
+
+        ImagePlus imp50MB = getImageWithDefinedHistogram(imageWidth, imageHeight, imageDepth, referenceHistogram, 0, 255, 8);
+
+        int numberOfBins = 256;
+
         for (int i = 0; i < 10; i++) {
-            int imageWidth = 1024;
-            int imageHeight = 1024;
-            int imageDepth = 50;
 
-            long[] referenceHistogram = new long[256];
-            referenceHistogram[2] = 34;
-            referenceHistogram[100] = 5;
-            referenceHistogram[145] = 22;
-            referenceHistogram[0] = imageWidth * imageHeight * imageDepth - sumArray(referenceHistogram);
-
-            ImagePlus imp50MB = getImageWithDefinedHistogram(imageWidth, imageHeight, imageDepth, referenceHistogram, 0, 255, 8);
-
-            int numberOfBins = 256;
-
+            // init GPU
             CLIJ clij = CLIJ.getInstance();
+            System.out.println("CLIJ running on " + clij.getGPUName());
 
             long sumTimeGPU = 0;
             long sumTimeCPU = 0;
 
+            // conversion to GPU
             sumTimeGPU += ElapsedTime.measureForceOutput("Conversion to GPU", () -> {
                 input = clij.convert(imp50MB, ClearCLBuffer.class);
             });
+            // conversion to imglib2
             sumTimeCPU += ElapsedTime.measureForceOutput("Conversion to CPU", () -> {
                 rai = ImageJFunctions.convertFloat(imp50MB);
             });
 
+            // measure min/max on GPU
             ElapsedTime.measureForceOutput("Min/max determination on GPU", () -> {
                 minGreyValue = new Double(Kernels.minimumOfAllPixels(clij, input)).floatValue();
                 maxGreyValue = new Double(Kernels.maximumOfAllPixels(clij, input)).floatValue();
             });
 
+            // determine histogram on GPU
             sumTimeGPU += ElapsedTime.measureForceOutput("Histogram on GPU", () -> {
                 Histogram.histogram(clij, input, minGreyValue, maxGreyValue, numberOfBins);
             });
+
+            // determine histogram on CPU
             sumTimeCPU += ElapsedTime.measureForceOutput("Histogram on CPU", () -> {
                 determineHistogram(rai, minGreyValue, maxGreyValue, numberOfBins);
             });
 
             System.out.println("SUM CPU: " + sumTimeCPU);
             System.out.println("SUM GPU: " + sumTimeGPU);
-
-            // checkImage(imp, referenceHistogram, 0f, 255f);
-
-
         }
     }
 
